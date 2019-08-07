1b28e61014c251b4cf795cb6ed4caa146c6020b5
Adam Anis <s3652122@student.rmit.edu.au>
2019-08-07 16:38:25 +1000

    created login and signup functionalities
    - created dummy homepage
    - added navigation bars to go to login and register pages
    - added login page and its function
    - added register page and its function
    - new users will be added to db
    - added dummy my profile page

---
+++ snazzysite/shop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'shop/home.html')
+
+def about(request):
+    return render(request, 'shop/about.html', {'title': 'About'})
---
+++ snazzysite/users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) # get form data after user input
+        if form.is_valid():
+            form.save() #save new user
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You may login')
+            return redirect('login')
+    else:
+        form = UserRegisterForm
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')            
