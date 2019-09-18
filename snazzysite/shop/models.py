5fc72aa528b7fc67600f30e08357b89e64210b71
Adam Anis <s3652122@student.rmit.edu.au>
2019-09-19 06:23:26 +1000

    reset database

--- snazzysite/shop/models.py
+++ snazzysite/shop/models.py
@@ -1,33 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+from django.utils.text import slugify
 
 class ProductStyle(models.Model):
-	product_style 		= models.CharField(max_length=100, blank=False, null=False)
+	description = models.CharField(max_length=100, blank=False, null=False)
 
 	def __str__(self):
-		return self.product_style
+		return self.description
 
 class ProductType(models.Model):
-	product_type		= models.CharField(max_length=100, blank=False, null=False) 
+	description	= models.CharField(max_length=100, blank=False, null=False) 
 
 	def __str__(self):
-		return self.product_type
+		return self.description
 
 class Product(models.Model):
-	name 	= models.CharField(max_length=100, blank=False, null=False)
-	desc 	= models.TextField()	
-	styleID = models.ForeignKey(ProductStyle, on_delete=models.CASCADE)
-	typeID 	= models.ForeignKey(ProductType, on_delete=models.CASCADE)
+	name 			= models.CharField(max_length=100, blank=False, null=False)
+	description 	= models.TextField()	
+	product_style 	= models.ForeignKey(ProductStyle, on_delete=models.SET_NULL, null=True)
+	product_type 	= models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True)
+	created_at		= models.DateTimeField(editable=False)
+	updated_at 		= models.DateTimeField(null=True)
+	slug 			= models.SlugField(unique=True)
 
 	def __str__(self):
 		return self.name
 
+	def save(self, *args, **kwargs):
+		if not self.pk:
+			self.created_at = timezone.now()
+		self.updated_at = timezone.now()
+		self.slug = slugify(self.name)
+		return super(Product, self).save(*args, **kwargs)
+
+	def get_absolute_url(self):
+		return reverse('product-detail', kwargs={'slug': self.slug}) # return full string path to product-detail
+
+	def get_add_to_cart_url(self):
+		return reverse('product-add-cart', kwargs={
+			'slug': self.slug
+		})
+
+	def get_remove_from_cart_url(self):
+		return reverse('product-remove-cart', kwargs={
+			'slug': self.slug
+		})	
+
 class ProductVariant(models.Model):
-	XS 	= "Extra Small"
-	S 	= "Small"
-	M 	= "Medium"
-	L 	= "Large"
-	XL 	= "Extra Large"
-	XXL = "Extra Extra Large"
+	XS 	= "XS"
+	S 	= "S"
+	M 	= "M"
+	L 	= "L"
+	XL 	= "XL"
+	XXL = "XXL"
 
 	SIZE_CHOICES = (
 		(XS, "Extra Small"),
@@ -38,82 +65,85 @@ class ProductVariant(models.Model):
 		(XXL, "Extra Extra Large"),
 	)
 
-	FEM = "Female"
-	MAL = "Male"
-	UNI = "Unisex"
+	FEM = "FEM"
+	MAL = "MAL"
+	UNI = "UNI"
 
 	GENDER_CHOICES = (
 		(FEM, "Female"),
 		(MAL, "Male"),
 		(UNI, "Unisex"),
-
 	)
 
-	color 		= models.CharField(max_length=100, blank=False, null=False)
-	size 		= models.CharField(max_length=100, choices=SIZE_CHOICES, blank=False, null=False)
-	gender 		= models.CharField(max_length=100, choices=GENDER_CHOICES, blank=False, null=False)
+	product		= models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_variants")
+	color 		= models.CharField(max_length=50, blank=False, null=False)
+	size 		= models.CharField(max_length=4, choices=SIZE_CHOICES, default=M, blank=False, null=False)
+	gender 		= models.CharField(max_length=3, choices=GENDER_CHOICES, default=UNI, blank=False, null=False)
 	quantity 	= models.PositiveIntegerField(blank=False, null=False)
 	price 		= models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
-	product		= models.ForeignKey(Product, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.product.name
 
 def product_images_path(instance, filename):
-	return 'product_images/{0}/{1}'.format(instance.product.id, filename)
+	return 'product_images/{0}/{1}'.format(instance.product.id, filename) # file will be uploaded to MEDIA_ROOT/product_<id>/<filename>
 
 class ProductImage(models.Model):
-	product 	= models.ForeignKey(Product, on_delete=models.CASCADE)
-	images 		= models.ImageField(upload_to=product_images_path)
+	product 	= models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
+	imagefile 	= models.ImageField(upload_to=product_images_path)
 
 	def __str__(self):
-		return self.product.name
+		return self.imagefile
 
 
 class OrderProduct(models.Model):
-	quantity 	= models.PositiveIntegerField(blank=False, null=False)
-	products	= models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+	# buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+	# ordered			= models.BooleanField(default=False)
+	quantity 	= models.PositiveIntegerField(default = 1, blank=False, null=False)
+	product_variant	= models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return self.id
+		return self.pk
+
+	def get_total_item_price(self):
+		return self.quantity * self.product_variant.price
 
 
 class Order(models.Model):
-	STD = "Standard Delivery"
-	EXP = "Express Delivery"
+	# STD = "STD"
+	# EXP = "EXD"
 
-	DEL_METHOD_CHOICES = (	
-		(STD, "Standard Delivery"),
-		(EXP, "Express Delivery"),
-	)
+	# DEL_METHOD_CHOICES = (	
+	# 	(STD, "Standard Delivery"),
+	# 	(EXP, "Express Delivery"),
+	# )
 
-	HOLD 		= "On Hold"
-	OTW 		= "Delivering"
-	DELIVERED 	= "Delivered"
+	# CC = "CC"
+	# PP = "PP"
 
-	DEL_STATUS_CHOICES = (
-		(HOLD, "On Hold"),
-		(OTW, "Delivering"),
-		(DELIVERED, "Delivered"),
-	)
+	# PAY_METHOD_CHOICES = (
+	# 	(CC, "Credit Card"),
+	# 	(PP, "PayPal"),
+	# )
 
-	CC = "Credit Card"
-	PP = "PayPal"
-
-	PAY_METHOD_CHOICES = (
-		(CC, "Credit Card"),
-		(PP, "PayPal"),
-
-	)
-
-	ref_no 			= models.CharField(max_length=100, blank=False, null=False)
-	ordered_date 	= models.DateTimeField(blank=False, null=False)
-	delivery_method = models.CharField(max_length=100, choices=DEL_METHOD_CHOICES, blank=False, null=False)
-	delivery_status = models.CharField(max_length=100, choices=DEL_STATUS_CHOICES, blank=False, null=False)
-	payment_method 	= models.CharField(max_length=100, choices=PAY_METHOD_CHOICES, blank=False, null=False)
-	payment_status 	= models.BooleanField(default=False)
-	total_price 	= models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
-	products		= models.ManyToManyField(OrderProduct)
+	buyer 			= models.ForeignKey(User, on_delete=models.CASCADE)
+	order_products	= models.ManyToManyField(OrderProduct)
+	start_date		= models.DateTimeField(auto_now_add=True)
+	ordered_date 	= models.DateTimeField(blank=True, null=True)
+	ordered			= models.BooleanField(default=False)
+	# ref_no 			= models.CharField(max_length=20, blank=True, null=True)	
+	# payment_method 	= models.CharField(max_length=2, choices=PAY_METHOD_CHOICES, default=CC, blank=True, null=True)
+	# delivery_method = models.CharField(max_length=3, choices=DEL_METHOD_CHOICES, default=STD, blank=True, null=True)
+	# total_price 	= models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+	# paid 			= models.BooleanField(default=False)
+	# shipped			= models.BooleanField(default=False)
 
 	def __str__(self):
-		return self.ref_no
+		return self.buyer.username
+
+	def get_total(self):
+		total = 0
+		for order_product in self.order_products.all():
+			total += order_product.get_total_item_price()
+		return total		
+
